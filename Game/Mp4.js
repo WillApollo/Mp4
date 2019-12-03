b702df8bea75c21facf73c8778c47878cb7cf776
IlstuWillAndrews <wrandr1@ilstu.edu>
2019-12-03 10:47:14 -0600

    Added Emily's Code

--- Game/Mp4.js
+++ Game/Mp4.js
@@ -1,17 +1,57 @@
 
+var myState = 0;
+
+var menuPic;
+var carPic;
+var robberPic;
+
+    function setup() {
+      createCanvas(1920, 1080);
+      menuPic = loadImage ("Images/introGirl.png");
+      carPic = loadImage ("Images/LadyinCar.png");
+      robberPic = loadImage ("Images/Robber1-01.jpg");
+    }
+
+    function preload() {
+
+    }
+    
+    function draw() {
+      //background("blue");
 
 
-function setup() {
-  // put setup code here
-}
-
-function draw() {
 
 
+      switch (myState) {
+
+        case 0:
+        //Menu
+        image(menuPic, 30, 100);
+
+          break;
+
+        case 1:
+        // Game dollar bill
+        image(robberPic, 100, 100);
+
+          break;
+
+        case 2:
+        // car dollar scene
+        image(carPic, 100, 100);
+
+          break;
+
+      }
+    }
+
+    function mouseReleased() {
+      myState++;
+
+      if (myState > 2) {
+        myState = 0;
+      }
 
 
-}
-
-function preload() {
-
-}
+    console.log(mouseX + "," + mouseY);   //take out later
+  }
