411b0edc977a67e63835564b1c0de1df4244d66b
IlstuWillAndrews <wrandr1@ilstu.edu>
2019-12-03 11:19:26 -0600

    Got State machine to work

--- Game/Mp4.js
+++ Game/Mp4.js
@@ -1,57 +1,62 @@
-
 var myState = 0;
 
 var menuPic;
 var carPic;
 var robberPic;
 
-    function setup() {
-      createCanvas(1920, 1080);
-      menuPic = loadImage ("Images/introGirl.png");
-      carPic = loadImage ("Images/LadyinCar.png");
-      robberPic = loadImage ("Images/Robber1-01.jpg");
-    }
+function setup() {
+  createCanvas(800, 800);
+  menuPic = loadImage("Assets/introGirl.png");
+  carPic = loadImage("Assets/LadyinCar.png");
+  robberPic = loadImage("Assets/Robber1-01.jpg");
 
-    function preload() {
+}
 
-    }
-    
-    function draw() {
-      //background("blue");
+function preload() {
+
+}
+
+function draw() {
+  //background("blue");
 
 
 
 
-      switch (myState) {
+  switch (myState) {
 
-        case 0:
-        //Menu
-        image(menuPic, 30, 100);
-
-          break;
-
-        case 1:
-        // Game dollar bill
-        image(robberPic, 100, 100);
-
-          break;
-
-        case 2:
-        // car dollar scene
-        image(carPic, 100, 100);
-
-          break;
-
-      }
-    }
-
-    function mouseReleased() {
-      myState++;
-
-      if (myState > 2) {
-        myState = 0;
+    case 0:
+      //Menu
+      image(menuPic, 30, 100, 800, 800);
+      if ((mouseX < 240) && (mouseX > 133) && (mouseY < 708) && (mouseY > 523)) {
+        mouseReleased();
       }
 
+      break;
 
-    console.log(mouseX + "," + mouseY);   //take out later
+    case 1:
+      // Game dollar bill
+
+      image(robberPic, 100, 100, 800, 800);
+
+      break;
+
+    case 2:
+      // car dollar scene
+      image(carPic, 100, 100, 800, 800);
+
+      break;
+
   }
+}
+
+function mouseReleased() {
+
+  myState++;
+
+  if (myState > 2) {
+    myState = 0;
+  }
+
+
+
+}
